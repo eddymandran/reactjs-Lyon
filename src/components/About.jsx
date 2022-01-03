67309e5f2c688dcdaca37ef1d9bb3e4774aee5f3
eddymandran <eddy.mandran@gmail.com>
2022-01-03 16:29:26 +0100

    Creation du composant About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+    const persons = [
+        { name: 'Khalid', age: 25 },
+        { name: 'Amel', age: 4 },
+        { name: 'Noam', age: 1 }
+    ];
+
+    return (
+        persons.map((person, index) => {
+            return (
+                <div key={index} className="card" style={{width: "18rem"}}>
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{person.name}</h5>
+                        <p className="card-text">{ person.age }</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            )
+        })
+    )
+}
+
+export default About;
