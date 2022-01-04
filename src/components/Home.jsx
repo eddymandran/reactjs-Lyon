ea06d82756a6492d03d53f2071f7962cffc8b6fb
eddymandran <eddy.mandran@gmail.com>
2022-01-04 11:48:41 +0100

    affichage produits dans home facon eddy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,28 @@
-const Home = () => <h3>Hello world</h3>
+import data from "../data/product.json"
+import {Link} from "react-router-dom";
+
+const Home = () => {
+    return (
+        <>
+            {data.map((product, index) => {
+                return (
+                    <div key={index} className="card" style={{width: "18rem"}}>
+                        <img src={product.picture} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{product.name}</h5>
+                            <p className="card-text">{ product.price } €</p>
+                            <p className="card-text">{ product.stock } in stock</p>
+                            <Link to={'/' + product.name} className="btn btn-primary">Go</Link>
+                        </div>
+                    </div>
+                )
+            })}
+        </>
+    )
+
+}
 
 export default Home;
+
+/** @TP */
+/** @todo Afficher le contenu du product.json dans notre Home en dupliquant le composant de About */
