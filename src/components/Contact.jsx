cf1a59f6f3056992abfe176c96b9cec9e28ed5ba
eddymandran <eddy.mandran@gmail.com>
2022-01-04 15:04:30 +0100

    formulaire

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
+
 const Contact = () => {
+
+    let [form, setForm] = useState({ name: 'Khalid', email: 'email@email.com', message: '' });
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(form);
+    }
+
+    const handleChange = (event) => {
+        const newData = { ...form };
+        const name = event.target.name;
+
+        newData[name] = event.target.value;
+
+        setForm(newData);
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" id="name" className="form-control" />
+                <input type="text" id="name" name="name" className="form-control"
+                       value={form['name']}
+                       onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="email" className="form-label">Email</label>
-                <input type="text" id="email" className="form-control" />
+                <input type="text" id="email" name="email" className="form-control"
+                       value={form['email']}
+                       onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="message" className="form-label">Message</label>
-                <textarea name="message" id="message" className="form-control" />
+                <textarea id="message" name="message" className="form-control"
+                          value={form['message']}
+                          onChange={handleChange} />
             </div>
 
             <div><input type="submit" value="Envoyer" className="btn btn-primary" /></div>
