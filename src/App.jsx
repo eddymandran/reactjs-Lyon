df56705005fbe643af2fcefc1dff1037fc2557a3
eddymandran <eddy.mandran@gmail.com>
2022-01-04 09:59:55 +0100

    mise en place 404 sur une URL precise

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import NotFound from './components/NotFound';
 import NavBar from './components/NavBar';
 import AboutName from './components/AboutName';
+import NotFoundAbout from "./components/NotFoundAbout";
 
 const App = () => {
     return (
@@ -17,8 +18,12 @@ const App = () => {
                         <Route path="/about" element={<About />} />
                         <Route path="/about/:name" element={<AboutName />} />
 
+                        {/* pour avoir une 404 sur une URL precise */}
+                        <Route path="/about/:name/*" element={<NotFoundAbout />} />
+
                         <Route path="/contact" element={<Contact />} />
 
+                        {/* pour avoir une 404 sur toutes les autres URL */}
                         <Route path="*" element={<NotFound />} />
                     </Routes>
                 </div>
---
+++ src/components/NotFoundAbout.jsx
@@ -0,0 +1,3 @@
+const NotFoundAbout = () => <h3>Resource not found for this about path!</h3>
+
+export default NotFoundAbout;
