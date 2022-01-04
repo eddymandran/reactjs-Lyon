2d1ab677ebe895fb35fcd2d60957ba62a5dbc800
eddymandran <eddy.mandran@gmail.com>
2022-01-04 11:54:59 +0100

    affichage produits dans home facon Khalid ( correction du key avec ID)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 const Home = () => {
     return (
         <>
-            {data.product.map((product, index) => {
+            {data.product.map((product, id) => {
                 return (
-                    <div key={index} className="card" style={{width: "18rem"}}>
+                    <div key={product.id} className="card" style={{width: "18rem"}}>
                         <img src={product.picture} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{product.name}</h5>
